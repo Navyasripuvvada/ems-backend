9e126ec7f40fa78af6c827e6aff7de18fab73cdd
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-06-10 16:41:24 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin:"http://localhost:3000",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
